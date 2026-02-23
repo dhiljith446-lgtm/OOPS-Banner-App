ad435667f6d2a2e925252f6b8379cdaf05aad548
Dhiljith Kumar <dhiljith446@gmail.com>
2026-02-23 20:17:37 +0530

    UC3: Refactor to use String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,27 @@
+/**
+ * OOPS Banner Application - Use Case 3
+ * ASCII art using String.join() for memory efficiency
+ * 
+ * @author Your Name
+ * @version 3.0
+ */
 public class OOPSBannerApp {
+    
     public static void main(String[] args) {
-        System.out.println("*****************************");
-        System.out.println("*                           *");
-        System.out.println("*           OOPS            *");
-        System.out.println("*       BANNER APP          *");
-        System.out.println("*                           *");
-        System.out.println("*****************************");
+        // Line 1: Top of letters
+        System.out.println(String.join("", 
+            "  ___    ___    ___    ___    ___  "));
+        
+        // Line 2: Upper middle
+        System.out.println(String.join("", 
+            " / _ \\  ", "/ _ \\  ", "/ _ \\  ", "/ _ \\  ", "/ _ \\ "));
+        
+        // Line 3: Middle
+        System.out.println(String.join("", 
+            "| (_) |", " (_) |", " (_) |", " (_) |", " (_) |"));
+        
+        // Line 4: Bottom
+        System.out.println(String.join("", 
+            " \\___/  ", "\\___/  ", "\\___/  ", "\\___/  ", "\\___/ "));
     }
 }
