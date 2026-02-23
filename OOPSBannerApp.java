c4c0db6216bf5d79a90034490707cd27fe1c3085
Dhiljith Kumar <dhiljith446@gmail.com>
2026-02-23 20:04:38 +0530

    UC2: Added banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,10 @@
-/**
- * OOPS Banner Application - Use Case 1
- * Prints "OOPS" to the console as the foundational step
- * 
- * @author Your Name
- * @version 1.0
- */
 public class OOPSBannerApp {
-    
-    /**
-     * Main method - entry point of the application
-     * @param args command line arguments (not used)
-     */
     public static void main(String[] args) {
-        // Print the literal text "OOPS" to the console
-        System.out.println("OOPS");
+        System.out.println("*****************************");
+        System.out.println("*                           *");
+        System.out.println("*           OOPS            *");
+        System.out.println("*       BANNER APP          *");
+        System.out.println("*                           *");
+        System.out.println("*****************************");
     }
 }
