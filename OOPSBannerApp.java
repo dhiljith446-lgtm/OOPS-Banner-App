a06831bafd299991b15dd47925dbf4d630db8387
Dhiljith Kumar <dhiljith446@gmail.com>
2026-02-23 21:30:20 +0530

    UC4: Implement array and loop for banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,17 @@
-/**
- * OOPS Banner Application - Use Case 3
- * ASCII art using String.join() for memory efficiency
- * 
- * @author Your Name
- * @version 3.0
- */
 public class OOPSBannerApp {
-    
     public static void main(String[] args) {
-        // Line 1: Top of letters
-        System.out.println(String.join("", 
-            "  ___    ___    ___    ___    ___  "));
         
-        // Line 2: Upper middle
-        System.out.println(String.join("", 
-            " / _ \\  ", "/ _ \\  ", "/ _ \\  ", "/ _ \\  ", "/ _ \\ "));
+        String[] bannerLines = {
+            String.join("", "*****************************"),
+            String.join("", "*", "                           ", "*"),
+            String.join("", "*", "           ", "OOPS", "            ", "*"),
+            String.join("", "*", "       ", "BANNER APP", "          ", "*"),
+            String.join("", "*", "                           ", "*"),
+            String.join("", "*****************************")
+        };
         
-        // Line 3: Middle
-        System.out.println(String.join("", 
-            "| (_) |", " (_) |", " (_) |", " (_) |", " (_) |"));
-        
-        // Line 4: Bottom
-        System.out.println(String.join("", 
-            " \\___/  ", "\\___/  ", "\\___/  ", "\\___/  ", "\\___/ "));
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
